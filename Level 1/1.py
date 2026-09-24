a0e9abb05b5dc98bab9019661e876fb81d96cb8a
Pavithra T R <pavithratr007@gmail.com>
2026-09-24 19:52:30 +0530

    Day 2 - Level 1 questions

---
+++ Level 1/1.py
@@ -0,0 +1,5 @@
+# print numbers from 1 to N
+
+n=int(input("enter n: "))
+for i in range(1,n+1):
+    print(i)
---
+++ Level 1/2.py
@@ -0,0 +1,5 @@
+# print numbers from N down to 1
+
+n=int(input("enter n: "))
+for i in range(n,0,-1):
+    print(i)
